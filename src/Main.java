c3acbc1def74493a20d4adcb7df6d31068a62304
id251 <danyalider178@gmail.com>
2023-02-22 19:53:16 +0300

    com

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,15 @@ import java.lang.reflect.Array;
 import java.lang.reflect.GenericArrayType;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static  void main(String[] args) {
-        ArrayList<Object> T=new ArrayList<>();
-                T.add("Hello");
-                T.add(12);
-                T.add('c');
-                T.add("World");
-        genericMethod(T.toArray());
+
+        Integer []array={12,23,34};
+        genericMethod(array);
     }
     private static <T> void genericMethod(T[] genericArray) {
-
         System.out.println(genericArray[0]);
         System.out.println(genericArray[genericArray.length -1]);
         System.out.println(Arrays.toString(genericArray));
